565649ebf19b50899ec26e9673ee7d4e78790d28
Sergey Golodyaev <gabadysergei@yandex.ru>
2021-02-13 08:05:55 +0300

    saving all

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file', type=str, help='file first')
 parser.add_argument('second_file', type=str, help='file second')
+parser.add_argument('-f', '--format', help='set format of output')
 
 args = parser.parse_args()
 
