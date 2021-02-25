b63ee8bc92cdc9cdf84a605bcda0e90274cc44df
Sergey Golodyaev <gabadysergei@yandex.ru>
2021-02-25 21:22:05 +0300

    saving all

--- gendiff/files.py
+++ gendiff/files.py
@@ -1,12 +1,22 @@
 import os.path
 import json
+import yaml
+
+FORMATS = {
+    "json": "json",
+    "yml": "yml",
+    "yaml": "yaml"
+}
 
 
 def read_file(file):
     abs_path = os.path.abspath(file)
     _, extension = os.path.splitext(abs_path)
     result = ''
-    with open(abs_path, 'r') as file_descriptor:
-        if extension == '.json':
-            result = json.load(file_descriptor)
+    with open(abs_path) as file_descriptor:
+        if extension == FORMATS["json"]:
+            return json.load(file_descriptor)
+        if extension == FORMATS["yml"] or format == FORMATS["yaml"]:
+            part = yaml.safe_load(file_descriptor)
+            return part
     return result
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -5,14 +5,20 @@ from gendiff.files import read_file
 # Test files
 FILE1 = 'tests/fixtures/filepath1.json'
 FILE2 = 'tests/fixtures/filepath2.json'
+FILE3 = 'tests/fixtures/filepath1.yml'
+FILE4 = 'tests/fixtures/filepath2.yml'
 
 
 # Answer files
 ANSWER = 'tests/fixtures/answer.txt'
 
 
-TEST_DATA = [
+TEST_DATA_JSON = [
     (FILE1, FILE2, ANSWER)
+]
+
+TEST_DATA_YAML = [
+    (FILE3, FILE4, ANSWER)
 ]
 
 
@@ -22,8 +28,17 @@ def read_txt(path_to_file):
     return expected.strip()
 
 
-def test_txt():
-    for (file1, file2, answer) in TEST_DATA:
+def test_json():
+    for (file1, file2, answer) in TEST_DATA_JSON:
+        file_one = read_file(file1)
+        file_two = read_file(file2)
+        expected = read_txt(answer)
+        result = str(generate_diff(file_one, file_two))
+        assert result == expected
+
+
+def test_yaml():
+    for (file1, file2, answer) in TEST_DATA_YAML:
         file_one = read_file(file1)
         file_two = read_file(file2)
         expected = read_txt(answer)
