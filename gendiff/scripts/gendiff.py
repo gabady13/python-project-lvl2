ed5c6b7aae4583e051c7c85be6b3b3e1061dbc10
Sergey Golodyaev <gabadysergei@yandex.ru>
2021-11-17 21:00:05 +0300

    refactoring cli

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
-import sys
-from gendiff.cli import run
+import argparse
+from gendiff.gendiff import generate_diff
 
 
 def main():
-    res = run(sys.argv[1:])
-    print(res)
+    """Print diff between two files."""
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('-f', '--format',
+                        metavar='FORMAT',
+                        help='set format of output',
+                        default='stylish')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+    print(generate_diff(args.first_file, args.second_file, args.format))
+
+
+if __name__ == '__main__':
+    main()
