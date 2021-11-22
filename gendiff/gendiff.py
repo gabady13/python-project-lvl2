20187edbf75853b393e001cf6c61ca2c2abebd26
Sergey Golodyaev <gabadysergei@yandex.ru>
2021-11-22 08:36:07 +0300

    refactoring

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -23,35 +23,34 @@ def get_data(source):
 
 def get_inner_diff(old_data, new_data, root_key=''):
     children = []
-    old_keys = set(old_data.keys())
-    new_keys = set(new_data.keys())
-    for key in new_keys - old_keys:
-        children.append({const.KEY_KEY: key,
-                         const.KEY_STATUS: const.STATUS_NEW,
-                         const.KEY_VALUE:
-                             {const.VALUE_NEW: new_data[key]}})
-    for key in old_keys - new_keys:
-        children.append({const.KEY_KEY: key,
-                         const.KEY_STATUS: const.STATUS_DEL,
-                         const.KEY_VALUE: {const.VALUE_DEL: old_data[key]}})
-    for key in old_keys & new_keys:
-        if isinstance(old_data[key], dict) and isinstance(new_data[key],
-                                                          dict):
-            stay_key = get_inner_diff(old_data[key], new_data[key], key)
+    keys = list(set(old_data.keys()) | set(new_data.keys()))
+    for key in sorted(keys):
+        if old_data.get(key) is None:
+            children.append({const.KEY_KEY: key,
+                            const.KEY_STATUS: const.STATUS_NEW,
+                            const.KEY_VALUE:
+                                {const.VALUE_NEW: new_data.get(key)}})
+        elif new_data.get(key) is None:
+            children.append({const.KEY_KEY: key,
+                             const.KEY_STATUS: const.STATUS_DEL,
+                             const.KEY_VALUE:
+                                 {const.VALUE_DEL: old_data.get(key)}})
+        elif isinstance(old_data.get(key), dict) \
+                and isinstance(new_data.get(key), dict):
+            stay_key = get_inner_diff(old_data.get(key), new_data.get(key), key)
         else:
             stay_key = {const.KEY_KEY: key}
-            if old_data[key] == new_data[key]:
+            if old_data.get(key) == new_data.get(key):
                 stay_key.update({const.KEY_STATUS: const.STATUS_STAY,
                                  const.KEY_VALUE:
                                      {const.VALUE_STAY: old_data[key]}})
             else:
                 stay_key.update({const.KEY_STATUS: const.STATUS_CHANGE,
                                  const.KEY_VALUE:
-                                     {const.VALUE_DEL: old_data[key],
-                                      const.VALUE_NEW: new_data[key]}})
-
-        children.append(stay_key)
-        children.sort(key=get_key)
+                                     {const.VALUE_DEL: old_data.get(key),
+                                      const.VALUE_NEW: new_data.get(key)}})
+            children.append(stay_key)
+            children.sort(key=get_key)
 
     res = {const.KEY_KEY: root_key,
            const.KEY_CHILDREN: children}
