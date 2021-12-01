c8198d5729589ffa9207bced20f26e605132906e
Sergey Golodyaev <gabadysergei@yandex.ru>
2021-12-01 20:10:21 +0300

    refactoring params

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+import argparse
 
 
 def parse_json(data):
@@ -8,3 +9,14 @@ def parse_json(data):
 
 def parse_yaml(data):
     return yaml.safe_load(data)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('-f', '--format',
+                        metavar='FORMAT',
+                        help='set format of output',
+                        default='stylish')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    return parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
 from gendiff.gendiff import generate_diff
-import argparse
+from gendiff.parsers import parse_args
 
 
 def main():
     """Print diff between two files."""
-    arg = args()
+    arg = parse_args()
     print(generate_diff(arg.first_file,
                         arg.second_file,
                         arg.format))
-
-
-def args():
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('-f', '--format',
-                        metavar='FORMAT',
-                        help='set format of output',
-                        default='stylish')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    return parser.parse_args()
 
 
 if __name__ == '__main__':
